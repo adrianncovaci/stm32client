5b06686d7634050472aa26993a4aa12da19fc5c8
Adrian Covaci <adrian.covaci@mantiq.com>
2024-02-09 19:01:35 +0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ enum Command<'a> {
     Info,
     Read,
     Erase { address: u32, length: u32 },
-    Write { data: &'a [u8] },
+    Write { sector: &'a [u8], data: &'a [u8] },
     Boot,
 }
 
@@ -29,6 +29,17 @@ pub enum BootloadError {
     FlashError,
     NetworkError,
     InternalError,
+    PartialWriteSuccess(u32),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ReadResponse<'a> {
+    pub bootloader_version: &'a [u8],
+    pub git_version: &'a [u8],
+    pub built_time: &'a [u8],
+    //pub mcu_id: String,
+    pub flash_bank1_len: usize,
+    pub flash_bank2_len: usize,
 }
 
 /// Interacts with a bootloader over the network
@@ -107,36 +118,38 @@ enum Commands {
 
 struct Client {
     socket: TcpStream,
+    chunk_size: u32,
 }
 
 impl Client {
-    fn new(hostname: &str, port: u16, timeout: u64) -> Result<Self, std::io::Error> {
+    fn new(
+        hostname: &str,
+        port: u16,
+        timeout: u64,
+        chunk_size: u32,
+    ) -> Result<Self, std::io::Error> {
         let socket = TcpStream::connect((hostname, port))?;
         socket.set_read_timeout(Some(Duration::from_secs(timeout)))?;
         socket.set_write_timeout(Some(Duration::from_secs(timeout)))?;
         //Just block for now as we only do one op at a time
         socket.set_nonblocking(false)?;
 
-        Ok(Self { socket })
+        Ok(Self { socket, chunk_size })
     }
 
-    fn send_program_request(
-        &mut self,
-        lma: u64,
-        binfile: PathBuf,
-        chunk_size: u64,
-    ) -> Result<(), std::io::Error> {
+    fn send_program_request(&mut self, lma: u64, binfile: PathBuf) -> Result<(), std::io::Error> {
         let mut binfile = std::fs::read(binfile).expect("Failed to read binfile");
 
         let len = binfile.len();
         let padding = if len % 32 == 0 { 0 } else { 32 - (len % 32) };
         binfile.resize(len + padding, 0xFF);
-        let segments = binfile.chunks(chunk_size as usize);
+        let mut segments = binfile.chunks(self.chunk_size as usize);
 
         println!("Erasing flash sector");
         self.erase_flash(0x08010000, len as u32)?;
 
         let segments_len = segments.len();
+
         for (i, segment) in segments.into_iter().enumerate() {
             println!(
                 "Writing segment(size={}) {} of {}",
@@ -144,7 +157,7 @@ impl Client {
                 i,
                 segments_len
             );
-            self.write_flash(segment)?;
+            self.write_flash(i as u32, segment).unwrap();
         }
 
         Ok(())
@@ -155,26 +168,50 @@ impl Client {
         let cmd = postcard::to_stdvec(&cmd).expect("Failed to serialize erase command");
         self.socket.write_all(&cmd)?;
 
-        println!("{:?} erasing the flash", self.get_reply().unwrap());
+        println!("{:?} erasing the flash", self.get_reply());
 
         Ok(())
     }
 
     fn get_reply(&mut self) -> Result<BootloadError, std::io::Error> {
-        let mut buf = vec![0; 4];
-        self.socket.read_to_end(&mut buf)?;
+        let mut buf = vec![0; 256];
+        self.socket.read(&mut buf)?;
         postcard::from_bytes(&buf).map_err(|_| {
             std::io::Error::new(std::io::ErrorKind::Other, "Failed to deserialize error")
         })
     }
 
-    fn write_flash(&mut self, data: &[u8]) -> Result<(), std::io::Error> {
-        let cmd = Command::Write { data };
+    fn write_flash(&mut self, sector: u32, data: &[u8]) -> Result<(), std::io::Error> {
+        let sector = &sector.to_le_bytes();
+        let cmd = Command::Write { sector, data };
         let cmd = postcard::to_stdvec(&cmd).expect("Failed to serialize write command");
         println!("Writing {} bytes to the socket", cmd.len());
+        self.socket.write_all(&cmd).unwrap();
+
+        println!("Writing status: {:?}", self.get_reply());
+
+        Ok(())
+    }
+
+    fn send_info_request(&mut self) -> Result<(), std::io::Error> {
+        let cmd = Command::Info;
+        let cmd = postcard::to_stdvec(&cmd).expect("Failed to serialize info command");
         self.socket.write_all(&cmd)?;
 
-        println!("Writing status: {:?}", self.get_reply()?);
+        //println!("Info reply: {:?}", self.get_reply()?);
+
+        //let mut buf = vec![0; 1024];
+        //self.socket.read_to_end(&mut buf)?;
+        //let respionse = postcard::from_bytes::<ReadResponse>(&buf)
+        //    .map(|el| el.clone())
+        //    .map_err(|_| {
+        //        std::io::Error::new(
+        //            std::io::ErrorKind::Other,
+        //            "Failed to deserialize info response",
+        //        )
+        //    })?;
+
+        //println!("Info response: {:?}", respionse);
 
         Ok(())
     }
@@ -182,21 +219,21 @@ impl Client {
 
 fn main() {
     let args = Cli::parse();
-    let mut client =
-        Client::new(&args.hostname, args.port, args.timeout).expect("Failed to connect");
+    let mut client = Client::new(
+        &args.hostname,
+        args.port,
+        args.timeout,
+        args.chunk_size as u32,
+    )
+    .expect("Failed to connect");
 
     match args.command {
         Commands::Info => {
-            let cmd = Command::Info;
-            let cmd = postcard::to_stdvec(&cmd).expect("Failed to serialize info command");
-            client
-                .socket
-                .write_all(&cmd)
-                .expect("Failed to send info command");
+            println!("Info {:?}", client.send_info_request())
         }
         Commands::Program { lma, binfile } => {
             client
-                .send_program_request(lma, binfile, args.chunk_size as u64)
+                .send_program_request(lma, binfile)
                 .expect("Failed to send program request");
         }
         Commands::Configure {
